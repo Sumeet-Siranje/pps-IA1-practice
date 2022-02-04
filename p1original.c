46924a92a1cd3d7d6a9aa7499ad64efc2073d93c
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-02-04 08:40:08 +0000

    p1original.c_done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-rdtdr
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter two numbers\n");
+  scanf("%d%d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int c)
+{
+  printf("sum of the two numbers %d and %d id = %d\n",a,b,c);
+}
+int main()
+{
+  int n1,n2,n3;
+  input(&n1,&n2);
+  add(n1,n2,&n3);
+  output(n1,n2,n3);
+  
 }
