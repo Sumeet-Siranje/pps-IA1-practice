95ab47709fa2afc1155e815638f0d684a59df119
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-02-05 10:31:06 +0000

    p1final_revesion

--- p1final.c
+++ p1final.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-void input(int *a,int *b){
-  printf("enter the first numbers\n");
-  scanf("%d",a);
-  printf("enter the second  numbers\n");
-  scanf("%d",b);
+void input(int *a, int *b){
+  printf("enter two numbers\n");
+  scanf("%d%d",a,b);
 }
 void add(int a,int b,int *sum){
-*sum=a+b;
+  *sum = a+b;
 }
 void output(int a,int b,int c){
-  printf("sum of %d and %d is %d\n",a,b,c);
+  printf("the sum of %d and %d is %d\n",a,b,c);
 }
-int main()
-{
+int main(){
   int n1,n2,n3;
   input(&n1,&n2);
   add(n1,n2,&n3);
   output(n1,n2,n3);
-  return 0;
+return 0;
 }
---
+++ p2final.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int input(){
+  int a,b,c;
+  printf("enter the three number\n");
+  scanf("%d%d%d",&a,&b,&c);
+}
