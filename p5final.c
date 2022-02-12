fefa9a8e75f75e6e6daac76648f73a1cec653863
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-02-12 07:01:04 +0000

    p5final.c_revesion

--- p5final.c
+++ p5final.c
@@ -1,29 +1,26 @@
 #include<stdio.h>
 float input(){
   float n;
-  printf("enter a number to find the square root of :\n");
+  printf("enter the squareroot of number\n");
   scanf("%f",&n);
   return n;
 }
-float squareroot(float n)
-{
-  float x =n;
-  float y=1;
-  float e=0.000001;
-  while (x-y>e)
-{
-x=(x +y)/2;
-y=n/x;
+float squareroot(float n){
+ float x=n;
+ float y=1;
+ float e=0.000001;
+ while(x-y>e){
+   x=(x+y)/2;
+   y=n/x;
+ } 
+ return x;
 }
-return x;
-}
-void output(float n,float sres)
-{
-  printf("squareroot of given input %0.2f is = %0.2f \n",n,sres);
+void output(float n,float sres){
+  printf("square root %0.2f is %0.2f\n",n,sres);
 }
 int main(){
-float n,sres;
-n=input();
-sres = squareroot(n);
-output(n,sres);
+float n=input();
+float p=squareroot(n);
+output(n,p);
+return 0;
 }
